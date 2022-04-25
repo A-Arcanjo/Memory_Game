1d33be0d1b729f66a55f9cc3c3d6c02d7001ad95
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2022-04-25 11:28:04 +0200

    home

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,80 +1,78 @@
-
-import React, {useState, useEffect, useContext} from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { ScoreContext } from "../App";
 import SingleCard from "./SingleCard";
 import { data } from "./cardsData";
 
 const Cards = (props) => {
-    //const { userData } = useContext(ScoreContext);
-    const [cards, setCards] = useState([]);
-    const [choiceOne, setChoiceOne] = useState(null)
-    const [choiceTwo, setChoiceTwo] = useState(null)
+  const { userData } = useContext(ScoreContext);
+  const [cards, setCards] = useState([]);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
-    // * SHUFFLE CARDS
-    const shuffleCards = () => {
-        const shuffledCards = [...data, ...data]
-        .sort(()=> Math.random() - 0.5)
-        .map((card) => ({ ...card, id: Math.random() }));
-  
-         setCards(shuffledCards)
-        
-    }
+  // * SHUFFLE CARDS
+  const shuffleCards = () => {
+    const shuffledCards = [...data, ...data]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
 
-    // * CHOOSE CARDS     
-    const choseCards = (card) => {
-        console.log(card);
-        choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
-    }
-    
-     // * MATCH CARDS
- useEffect(()=>{
-    if(choiceOne && choiceTwo){
-        if(choiceOne.color === choiceTwo.color){
-            setCards(previousCards => {
-                return previousCards.map(card => {
-                    if(card.color === choiceOne.color) {
-                        return {...card, matched:true}
-                    } else {
-                        return card
-                    }
-                })
-            })
-            setChoiceOne(null)
-            setChoiceTwo(null) 
-        } else {
-            setChoiceOne(null)
-            setChoiceTwo(null)  }                    
-    }
-},[choiceOne, choiceTwo])
+    setCards(shuffledCards);
+  };
 
- console.log(cards);
-    return (
-        <div>
-            <div className="container_load">
-                <div className="form_container">
-                    <div className="heading_container">
-                        <h2>Player: </h2>
-                        {/* {userData.playerName} */}
-                    </div>
+  // * CHOOSE CARDS
+  const choseCards = (card) => {
+    console.log(card);
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  // * MATCH CARDS
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.color === choiceTwo.color) {
+        setCards((previousCards) => {
+          return previousCards.map((card) => {
+            if (card.color === choiceOne.color) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        setChoiceOne(null);
+        setChoiceTwo(null);
+      } else {
+        setChoiceOne(null);
+        setChoiceTwo(null);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  console.log(cards);
+  return (
+    <div>
+      <div className="container_load">
+        <div className="form_container">
+          <div className="heading_container">
+            <h2>Player: {userData.playerName}</h2>
+          </div>
           <div className="button_container">
             <button onClick={shuffleCards}>Load Cards</button>
           </div>
-        <div>
+          <div>
             <div className="cards-container">
-                {cards.map(card => (
-                     <SingleCard 
-                     key={card.id}
-                     card={card}
-                     color={card.color}
-                     choseCards={choseCards}
-                     />
-                ))}
+              {cards.map((card) => (
+                <SingleCard
+                  key={card.id}
+                  card={card}
+                  color={card.color}
+                  choseCards={choseCards}
+                />
+              ))}
             </div>
+          </div>
         </div>
-        </div>
-        </div>
+      </div>
     </div>
-    )
-}
+  );
+};
 
 export default Cards;
